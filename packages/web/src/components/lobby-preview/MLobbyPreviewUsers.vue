7f38be7315a1e374bddb1a5b3aef29de51b3034b
JacksonBowe <jacksonbowe98@gmail.com>
2024-03-17 14:46:04 +1000

    switching to composable

--- packages/web/src/components/lobby-preview/MLobbyPreviewUsers.vue
+++ packages/web/src/components/lobby-preview/MLobbyPreviewUsers.vue
@@ -4,10 +4,10 @@
 			Players
 		</MCardHeader>
 		<q-separator color="primary" inset />
-		<MCardContent class="col row q-py-xs" v-if="users?.length">
-			<MLobbyPreviewUsersList :users="users?.slice(0, 8)" />
+		<MCardContent class="col row q-py-xs" v-if="selectedLobby?.users?.length">
+			<MLobbyPreviewUsersList :users="selectedLobby?.users?.slice(0, 8)" />
 			<q-separator vertical color="accent" class="q-mx-sm" inset />
-			<MLobbyPreviewUsersList :users="users?.slice(8, 15)" />
+			<MLobbyPreviewUsersList :users="selectedLobby?.users?.slice(8, 15)" />
 		</MCardContent>
 	</MCard>
 </template>
@@ -17,21 +17,8 @@ import { MCard, MCardHeader, MCardContent } from '../ui/card';
 
 import MLobbyPreviewUsersList from './MLobbyPreviewUsersList.vue';
 
-import { useLobbyStore } from 'src/stores/lobby';
-import { useLobbies } from 'src/composables';
-import { computed, ComputedRef } from 'vue';
-import { type LobbyUser } from 'src/api/lobby';
+import { useSelectedLobby } from 'src/composables';
 
-const lStore = useLobbyStore();
-const { data: lobbies } = useLobbies();
 
-const users: ComputedRef<LobbyUser[] | null> = computed(() => {
-	if (lStore.selectedLobbyId && lobbies.value) {
-		const lobby = lobbies.value.find((lobby) => lobby.id === lStore.selectedLobbyId);
-		if (lobby && lobby.users) {
-			return lobby.users.concat(Array(15).fill({}).slice(0, 15));
-		}
-	}
-	return [];
-});
+const { selectedLobby } = useSelectedLobby();
 </script>
