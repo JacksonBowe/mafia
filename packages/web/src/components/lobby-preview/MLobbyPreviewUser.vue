7df99edfa765628b9b078cd4a6ffbf5eded1a8e8
JacksonBowe <jacksonbowe98@gmail.com>
2024-07-10 18:19:42 +1000

    fixing lobby preview user text wrapping

--- packages/web/src/components/lobby-preview/MLobbyPreviewUser.vue
+++ packages/web/src/components/lobby-preview/MLobbyPreviewUser.vue
@@ -1,15 +1,22 @@
 <template>
-	<q-item v-if="user.username" style="padding: 0px" clickable @click="click">
-		<q-item-section>
-			<q-item-label class="ellipsis content-center flex">
-				<q-icon
-					v-if="isHost"
-					name="star"
-					class="q-mr-xs"
-					color="yellow-4"
-				/>
-				{{ user.username }}</q-item-label
-			>
+	<q-item
+		v-if="user.username"
+		style="padding: 0px; border-radius: 8px"
+		clickable
+		@click="click"
+	>
+		<q-item-section no-wrap>
+			<q-item-label class="ellipsis content-center flex" lines="1">
+				<div class="q-px-xs">
+					<q-icon
+						v-if="isHost"
+						name="star"
+						class="q-mr-xs"
+						color="yellow-4"
+					/>
+					{{ user.username }}
+				</div>
+			</q-item-label>
 		</q-item-section>
 	</q-item>
 </template>
@@ -27,4 +34,3 @@ const click = () => {
 	window.navigator.clipboard.writeText(props.user.id);
 };
 </script>
-src/lib/api/lobby
