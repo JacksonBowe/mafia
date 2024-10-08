fa0e0cba81dbf0583962936c56e8806eb5e0d7c1
JacksonBowe <jacksonbowe98@gmail.com>
2024-10-08 17:34:59 +1000

    init uv

--- stacks/LambdaLayers.ts
+++ stacks/LambdaLayers.ts
@@ -2,29 +2,24 @@ import { StackContext } from "sst/constructs";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 
 export function LambdaLayers({ app, stack }: StackContext) {
+	// aws-lambda-powertools
+	const powertools = lambda.LayerVersion.fromLayerVersionArn(
+		stack,
+		"lambda-powertools",
+		`arn:aws:lambda:${stack.region}:017000801446:layer:AWSLambdaPowertoolsPythonV3-python312-x86:1`
+	);
 
-    // aws-lambda-powertools
-    const powertools = lambda.LayerVersion.fromLayerVersionArn(
-        stack, "lambda-powertools", 
-        `arn:aws:lambda:${stack.region}:017000801446:layer:AWSLambdaPowertoolsPythonV2:60`
-    );
+	// requests
+	const requests = lambda.LayerVersion.fromLayerVersionArn(
+		stack,
+		"requests",
+		`arn:aws:lambda:${stack.region}:770693421928:layer:Klayers-p312-requests:1`
+	);
 
-    // requests
-    const requests = lambda.LayerVersion.fromLayerVersionArn(
-        stack, "requests", 
-        `arn:aws:lambda:${stack.region}:770693421928:layer:Klayers-p312-requests:1`
-    );
+	app.addDefaultFunctionLayers([powertools]);
 
-    // pydandic
-	const pydantic = new lambda.LayerVersion(stack, "pydantic", {
-		code: lambda.Code.fromAsset("packages/functions/layers/pydantic"),
-	});
-
-    app.addDefaultFunctionLayers([powertools, pydantic])
-    
-    return {
-        powertools,
-        requests,
-        pydantic
-    }
+	return {
+		powertools,
+		requests,
+	};
 }
